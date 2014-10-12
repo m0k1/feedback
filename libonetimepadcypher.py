412d2c3ead09b7a9c7f56e45d9ea4a3a7daf2832
Aleksandar Buza <public.hydra@gmail.com>
2014-10-12 11:53:39 +0200

    lib

--- libonetimepadcypher.py
+++ libonetimepadcypher.py
@@ -1,25 +1,27 @@
-blocksize = 65536
+block_size = 65536
 
-def filesize(filename):
+def file_size(filename):
     filename.seek(0,2)
     num = filename.tell()
     filename.seek(0,0)
     return num
+    
 def cypher(data,key):
     cyphertext = ''.join([chr(ord(x)^ord(y)) for x,y in zip(data,key)])
     return cyphertext
-def filecypher_keyfile(inputfilename,outputfilename,keyfilename):
+    
+def file_cypher_keyfile(inputfilename,outputfilename,keyfilename):
     try:
         inputfile = open(inputfilename,'r')
         outputfile = open(outputfilename,'w')
         keyfile = open(keyfilename,'r')
     except:
         return False
-    if filesize(inputfile) > filesize(keyfile):
+    if file_size(inputfile) > file_size(keyfile):
         return False
     cyphertext = ''
     while True:
-        data = inputfile.read(len(blocksize))
+        data = inputfile.read(len(block_size))
         if not data:
             break
         pad = keyfile.read(len(data))
